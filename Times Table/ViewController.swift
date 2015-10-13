a2393fee3eba3a4b05f1b529d9722e3451376358
Nicolas5150 <Nicolas5150@gmail.com>
2015-10-13 01:56:18 -0400

    Master Copy

--- Times Table/ViewController.swift
+++ Times Table/ViewController.swift
@@ -8,8 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
 
+    
+    @IBOutlet var table: UITableView!
+    
+    @IBAction func sliderMoved(sender: AnyObject)
+    {
+        // reload data makes the tableview funcs rerun and keeps the table value at 20 but gets the slider value and returns the new values into each cell
+        table.reloadData()
+        //print("\(sliderValue) \n");
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 20;
+    }
+    
+    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
+    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
+    
+    @available(iOS 2.0, *)
+    // this function will run 20 times (b/c of above return value from tableview func. It then will add the times table of the slider at the time to each of the 20 cells
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell");
+        // get the value of the slider and * 20 to get max value of table
+        // table will go from (.005*20 = 1-20) to (1*20 = 20-400)
+        let timesTable = Int(sliderValue.value * 20);
+        // convert to a string (also do +1 to remove 0 from begining rows
+        cell.textLabel?.text = String(timesTable * (indexPath.row + 1))
+        return cell;
+    }
+    
+    @IBOutlet var sliderValue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
